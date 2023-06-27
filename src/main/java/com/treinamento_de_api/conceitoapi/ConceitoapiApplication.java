0cc54afe5f596e0085c7f07286694e7cc276f49c
João Perdro Pires <joaopedropires61@gmail.com>
2023-06-26 22:46:52 -0300

    Projeto criado com as dependencias Spring Web, Spring Boot DevTools, Lombork e MYSQL CONECTOR

---
+++ src/main/java/com/treinamento_de_api/conceitoapi/ConceitoapiApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento_de_api.conceitoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceitoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceitoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamento_de_api/conceitoapi/ConceitoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento_de_api.conceitoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceitoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
